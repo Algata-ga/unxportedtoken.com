b8d2ad0d0edaf4626aad0ac37ffe1a5a0c079d7d
KKharidev <kkharidev@gmail.com>
2022-06-29 23:52:52 +0530

    footer completed no links added

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const date = new Date();
+const year = date.getFullYear();
+const copyright =
+    year +
+    '<strong><a href="#"> UNEXPORTED </a></strong>';
+document.getElementById("copyright").innerHTML = copyright;
+
+
+
+function nav() {
+  var element = document.getElementById("links");
+  element.classList.toggle("open");
+} 
