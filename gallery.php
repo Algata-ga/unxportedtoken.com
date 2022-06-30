ee2c7f71a278c02032690bb05b797a5a961f8da6
KKharidev <kkharidev@gmail.com>
2022-07-01 00:55:52 +0530

    gallery pade responsive

--- gallery.php
+++ gallery.php
@@ -21,7 +21,7 @@ function renderImages(){
         if($isImage){
             $SRC=$file_path;
             $TITLE=pathinfo($file_path,PATHINFO_FILENAME);
-            $VIDEOS_CONTAINER = "<div><video src='$SRC'></video><h4>$TITLE</h4></div>";
+            $VIDEOS_CONTAINER = "<div class='banner'><video src='$SRC'></video><h3 class='article-content' >'$TITLE'</h3></div>";
             $renderedImages = $renderedImages.$VIDEOS_CONTAINER;
         }
     }
@@ -45,7 +45,7 @@ function renderImages(){
     <!--/>-->
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
     <link rel="stylesheet" href="./styles/styles.css" />
-    <link rel="stylesheet" href="./styles/index.css" />
+    <link rel="stylesheet" href="./styles/gallery.css" />
     <script src="https://kit.fontawesome.com/025c12b0bf.js" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="./assets/logo.svg" type="image/x-icon">
     <title>UNXPORTED</title>
@@ -54,13 +54,15 @@ function renderImages(){
 <body>
     <header>
         <nav class="contain">
-            <div class="nav-logo">
-                <img src="./assets/logo.svg" alt="logo">
-                <h3 class="company-name">UNXPORTED</h3>
-            </div>
+        <a href="/">
+                <div class="nav-logo">
+                    <img src="./assets/logo.svg" alt="logo">
+                    <h3 class="company-name">UNXPORTED</h3>
+                </div>
+            </a>
             <div id="links" class="link-box">
                 <ul>
-                    <li>Home</li>
+                <li><a href="/">Home</a></li>
                     <li>About</li>
                     <li>Team</li>
                     <li><button class="bg-button">Whitepaper</button></li>
@@ -73,11 +75,28 @@ function renderImages(){
             </div>
         </nav>
     </header>
-    <div>
-    <?php 
-        renderImages(); 
-    ?>
+    <div class="latest">
+        <div class="content-box contain">
+            <h1 class="heading-left">latest</h1>
+            <div class='video-contain'>
+                <?php 
+                renderImages(); 
+            ?>
+</div>
+        </div>
     </div>
+
+    <footer>
+        <div id="copyright"></div>
+        <div class="flinks">
+            <div class="link"><i class="fa-brands fa-facebook"></i></div>
+            <div class="link"><i class="fa-brands fa-twitter"></i></i></div>
+            <div class="link"></i><i class="fa-brands fa-instagram"></i></div>
+            <div class="link"><i class="fa-brands fa-youtube"></i></i></div>
+        </div>
+    </footer>
+
     <script src="./js/index.js"></script>
 </body>
+
 </html>
