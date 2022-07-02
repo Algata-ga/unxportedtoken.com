a9a67354fcaa22d4fb171543ebd32c13a50fb5f4
Alfred Jophy <alfredjophy@protonmail.com>
2022-07-02 09:28:23 +0530

    Added modal for wallets

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
 const date = new Date();
 const year = date.getFullYear();
-const copyright =
-    year +
-    '<strong><a href="#"> UNXPORTED </a></strong>';
+const copyright = year + '<strong><a href="#"> UNXPORTED </a></strong>';
 document.getElementById("copyright").innerHTML = copyright;
 
-
-
 function nav() {
-  var element = document.getElementById("links");
-  element.classList.toggle("open");
-} 
+    var element = document.getElementById("links");
+    element.classList.toggle("open");
+}
+
+const close_modal = (id) => {
+    const modal = document.getElementById(id);
+    document.body.dataset.scrollable = true;
+    modal.dataset.toggle = false;
+};
+
+const open_modal = (id) => {
+    const modal = document.getElementById(id);
+    document.body.dataset.scrollable = false;
+    modal.dataset.toggle = true;
+};
+
